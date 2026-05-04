0f6183f9aa82bb2d96d4508bd9c2bf380369bfb3
jw2598 <jw208@dongyang.ac.kr>
2026-05-05 00:28:59 +0900

    feat: 튜터링 베이스

---
+++ src/main/java/com/dasom/dasom_tutoring_05/DasomTutoring05Application.java
@@ -0,0 +1,13 @@
+package com.dasom.dasom_tutoring_05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DasomTutoring05Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DasomTutoring05Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/dasom/dasom_tutoring_05/DasomTutoring05ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dasom.dasom_tutoring_05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DasomTutoring05ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
